3512a99dedba3ea66e8b0d0b2037bddc53c64fb5
unknown <kalinowskixmarcin@o2.pl>
2018-01-15 17:01:10 +0100

    versioning of scripts file

--- data/footer.php
+++ data/footer.php
@@ -2,7 +2,7 @@
 <?php if (isset($error)) foreach ($error as $value) echo "<b>! Error - $value</b><br>"; ?>
 </pre>
 </main>
-<script src="js/scripts.js"></script>
+<script src="js/scripts.js?v=<?php echo md5_file('js/scripts.js'); ?>"></script>
 
 </body>
 </html>
