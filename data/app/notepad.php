cb0c938754a9467aa52ec58b573049cc39009368
unknown <kalinowskixmarcin@o2.pl>
2018-02-19 09:58:13 +0100

    notes code change to global use

--- data/app/notepad.php
+++ data/app/notepad.php
@@ -1,6 +1,6 @@
 <section id="notepad">
 
-  <div class="note_area">
+  <div class="note_element">
     <textarea placeholder="Zacznij wpisywać tekst aby dodać nową notatkę"></textarea>
   </div>
 <?php
@@ -8,7 +8,7 @@
 foreach ($sql_pdo->query('SELECT * FROM `notes` ORDER BY `id` DESC') as $value) :
  ?>
 
-<div class="note_area">
+<div class="note_element">
   <textarea data-note="<?php echo $value['id']; ?>"><?php echo $value['txt']; ?></textarea>
 </div>
 
