05d7b86fbe4b2bc236083cc97872bf21d8233c4b
unknown <kalinowskixmarcin@o2.pl>
2018-02-20 11:58:17 +0100

    addded system class and error method within

---
+++ data/core/system.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * System Interface controller
+ */
+class System {
+
+  public function error($value) {
+    global $error;
+    $error[] = $value;
+    // echo "<pre class='error'>! Error - $value</pre>";
+  }
+
+}
+
+?>
--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
-//print_r ($_GET);
+
 if (!include("data/init.php")) { echo "<p>[!] Błąd krytyczny systemu - nie można zaimplementować pliku inicjującego.</p><p>[!] die();</p>"; die();  } //pobiera funkcje
 if (!include("data/functions.php")) { echo "<p>[!] Błąd krytyczny systemu - nie można zaimplementować pliku z funkcjami.</p><p>[!] die();</p>"; die();  } //pobiera funkcje
 if (!include("data/db.php")) { echo "<p>[!] Błąd krytyczny systemu - nie można zaimplementować pliku z połączeniem SQL.</p><p>[!] die();</p>"; die();  } //pobiera funkcje
 if (!include('data/core/router.php')) { echo "<p>[!] Błąd krytyczny systemu - nie można zaimplementować pliku z routingiem.</p><p>[!] die();</p>"; die();  } //pobiera funkcje
 
+if (!include("data/core/system.php")) { echo "<p>[!] Błąd krytyczny systemu - nie można zaimplementować pliku z klasą obsługi systemu.</p><p>[!] die();</p>"; die();  } //pobiera funkcje
 if (!include("data/core/database.php")) { echo "<p>[!] Błąd krytyczny systemu - nie można zaimplementować pliku z połączeniem SQL.</p><p>[!] die();</p>"; die();  } //pobiera funkcje
 if (!include("data/core/defaultController.php")) { echo "<p>[!] Błąd krytyczny systemu - nie można zaimplementować pliku z klasą dla kontrolerów.</p><p>[!] die();</p>"; die();  } //pobiera funkcje
 
