ba9c9f45adb2e879a0f0a7fc635929a9ce6ffa9a
manio4444 <kalinowskixmarcin@o2.pl>
2021-02-06 14:32:59 +0100

    calendar add saveEvent to api

--- data/class/calendar.php
+++ data/class/calendar.php
@@ -7,7 +7,9 @@ class calendar extends defaultController {
   public $sqlReturn;
 
   public function __construct() {
-    if (null === $this->getInstance()) {
+    $this->dbInstance = $this->getInstance();
+
+    if (null === $this->dbInstance) {
       System::error('Klasa "' . get_class() . '" Nie ma dostępu do połączenia SQL');
       return false;
     }
@@ -18,6 +20,35 @@ class calendar extends defaultController {
     $query = $this->getInstance()->query('SELECT * FROM `calendar_static` WHERE `data` > CURRENT_TIMESTAMP ORDER BY `calendar_static`.`data` ASC LIMIT '.$limit.'');
 
     return $query;
+
+  }
+
+  protected function saveEvent() {
+    if (
+      !$this->existsParam('txt')
+      || !$this->existsParam('data')
+    ) {
+      return $this->error404('Nie wprowadzono wymaganych danych');
+    }
+    if (empty($this->requestData['txt'])) {
+      return $this->error404('Pole tekst nie może być puste');
+    }
+    if (empty($this->requestData['data'])) {
+      return $this->error404('Pole data nie może być puste');
+    }
+
+    $sqlObj = $this->dbInstance->prepare( 'INSERT INTO `calendar_static` (`txt`, `data`) VALUES (:txt, :data)');
+    $sqlObj->bindValue(':txt', $this->requestData['txt'], PDO::PARAM_STR);
+    $sqlObj->bindValue(':data', $this->requestData['data'], PDO::PARAM_STR);
+    $sqlObj->execute();
+    $EventId = $this->dbInstance->lastInsertId();
+    $sqlReturn = $this->getInstance()->query('SELECT * FROM `calendar_static` WHERE `id` = ' . $EventId);
+    $lastInsertElement = $sqlReturn->fetch(PDO::FETCH_ASSOC);
+
+    return array(
+      'message' => 'Utworzono nowe wydarzenie',
+      'newElement' => $lastInsertElement,
+    );
 
   }
 }
